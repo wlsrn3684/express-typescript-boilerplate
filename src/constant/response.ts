e8797f7a7658e7a38774e00425497e4362bf3db4
wlsrn3684 <wlsrn3684@naver.com>
2022-08-11 16:13:13 +0900

    Initialize project

---
+++ src/constant/response.ts
@@ -0,0 +1,16 @@
+export const ResponseCode = {
+  ok: 200,
+  created: 201,
+  noContent: 204,
+
+  badRequest: 400,
+  invaildParameterType: 401,
+  missingParameter: 402,
+  forbidden: 403,
+  unauthorized: 404,
+  invaildPasswd: 411,
+  invaildRequest: 420,
+  confilct: 409,
+
+  internalError: 500,
+}
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import setting from './lib/setting'
+import logging from './lib/logging'
+import envConfig from './config/env'
+import log from './module/log'
+
+async function main() {
+  const PORT = envConfig.PORT ?? 4000
+
+  const server = express()
+
+  server.listen(PORT, async () => {
+    log.info(`Start server - 0.0.0.0:${PORT}`)
+  })
+}
+
+main()
+
+process.on('SIGINT', () => {
+  console.log()
+  log.info(`Quit Sever`)
+  process.exit()
+})
